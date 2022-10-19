1936d1fb8fc1c854319e7904681a9ab3038a0456
sifloz <hecn@live.com.mx>
2022-10-19 10:45:21 -0500

    Sesion 08 - Parte 2

--- sesion-08/sesion8.js
+++ sesion-08/sesion8.js
@@ -1,8 +1,27 @@
 var button = document.querySelector("button");
+var pre = document.querySelector("#data");
+var loadingText = document.querySelector("#loading");
+
+var data = {
+    currentDate: new Date(),
+    hours: new Date().getHours(),
+    minutes: new Date().getMinutes(),
+    seconds: new Date().getSeconds(),
+};
+
+var getData = function() {
+    console.log("Call API...");
+    loadingText.style.display = "block";
+    setTimeout(() => {
+        loadingText.style.display = "none";
+        pre.appendChild(document.createTextNode(JSON.stringify(data)))
+    }, 2000);
+}
 
 var printMessage = function() {
     button.innerText = "Button clicked"
     console.log("Button clicked")
+    getData();
     button.removeEventListener("click", printMessage)
 }
 
